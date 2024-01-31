5defc79b5f050adb405db540fc40f6be6ec38492
Tobias Schmidt <106234803+Piece-Of-Schmidt@users.noreply.github.com>
2024-01-31 16:18:37 +0100

    Update 1 clean_corpus.R

--- 1 clean_corpus.R
+++ 1 clean_corpus.R
@@ -39,15 +39,42 @@ corpus = filterID(corpus, corpus$meta$id[corpus$meta$resource %in% quellen_list]
 
 most_frquent_titles = table(corpus$meta$title)[1:20]
 most_frquent_titles
+# -> zeigt an, welches die haeufigsten Dokumenten-Titel sind. Oft Rubriken, manchmal Leserbriefe etc.
+# -> Oft ist es ratsam, solche Dokumente von der Analyse auszuschliessen
 
-pattern = "Life" # Begriffe (Patternsuche), durch "|" getrennt
+pattern = "Life|Death" # Begriffe (Patternsuche), durch "|" ("oder") getrennt
 filterTitles(corpus, pattern, print=T)
 
 # Doks ausschließen, bei denen <pattern> im Title vorkommt
-corpus = filterTitles(corpus, pattern)
+corpus_without_Life_or_Death = filterTitles(corpus, pattern)
 
 
 
 # sonst... ----------------------------------------------------------------
 
+
+
+# -------------------------------------------------------------------------
+# Corpus auf Artikel einschraenken, in denen ein bestimmtes Wort/Pattern vorkommt
+# -------------------------------------------------------------------------
+
+
+# Auf Grundlage des Vorkommens bestimmter Worte
+corpus = filterWord(corpus, "America") # -> Wichtig: per Default fuehrt die Funktion sog. Pattern Searches aus. D.h., es werden auch solche Dokumente gefunden, in denen (Beispiel hier) "American Dream" vorkommt 
+corpus_America = filterWord(corpus, "\\America\\b") # -> Um das zu umgehen: "\\b" stellt sicher, dass "America" zu beiden Seiten von einer Word Boundary (Leer- oder Satzzeichen) umgeben ist
+
+# Die Funktion filterWord() versteht zu einem gewissen Teil regular expressions
+# Es sind also auch Ausdruecke wie der Folgende moeglich:
+corpus_America = filterWord(corpus, "\\b(America|USA|United States)\\b", ignore.case=T)
+
+# -> "|" bedeutet "oder", "\\b" bedeutet "word boundary"
+# -> "ignore.case=T" stellt sicher, dass Gross- und Kleinschreibung ignoriert werden. Wenn das nicht gewuenscht ist, Parameter auf F (FALSE) setzen
+# -> Wichtig: "ignore.case=T" transformiert alle Texte automatisch in Kleinbuchstaben. Wenn das vermieden werden soll, ist der folgende Code zu empfehlen:
+
+mask = filterWord(corpus, "\\b(America|USA|United States)\\b", ignore.case=T, out="bin") # -> out="bin" gibt keinen Korpus, sondern einen booleschen Vektor zurueck
+relevant_ids = names(corpus$text)[mask]
+corpus_America = filterID(corpus, relevant_ids)
+
+# Fuer weitere Moeglichkeiten der Korpus-Einschraenkung (inkl. Einstellung, dass einzelne Worte mehrfach vorkommen sollen):
+# Siehe Koppers et al. (2021) tosca Vignette
 
