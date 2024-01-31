d4824450a7a430a01f965d8b08dd0208c13f5315
Tobias Schmidt <106234803+Piece-Of-Schmidt@users.noreply.github.com>
2024-01-31 15:48:14 +0100

    Update 0 functions.R

--- 0 functions.R
+++ 0 functions.R
@@ -1,11 +1,18 @@
-# load packages
-require(lubridate, quietly = T)
-require(tosca, quietly = T)
-require(writexl, quietly = T)
-require(dplyr, quietly = T)
-require(utf8, quietly = T)
-require(ggplot2, quietly = T)
+# install and/or load packages
 
+packages = c("lubridate","tosca","writexl", "dplyr", "utf8", "ggplot2")
+for(package in packages){
+  
+  load_package = T
+  if(!require(package, quietly=T, character.only=T)){
+    
+    cat("Package", package, "wird benoetigt, ist aber nicht installiert. Soll es installiert werden?\n0: nein\n1: ja")
+    install = as.logical(as.numeric(readline("Print 0 or 1:  ")))
+    
+    if(install) install.packages(package, dependencies = TRUE) else load_package = F
+  }
+  if(load_package){ library(package, character.only=TRUE) }
+}
 
 # -------------------------------------------------------------------------
 # KORPUS CLEANING
