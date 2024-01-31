f74334614662243d1ccea649a60ee5ae2b9fb9a0
Tobias Schmidt <106234803+Piece-Of-Schmidt@users.noreply.github.com>
2024-01-31 16:03:22 +0100

    Update 1 clean_corpus.R

--- 1 clean_corpus.R
+++ 1 clean_corpus.R
@@ -1,3 +1,4 @@
+require(tosca)
 
 # set wd
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
@@ -5,18 +6,19 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 # load functions
 source("0 functions.R")
 
+corpus = readRDS("example_corpus.rds")
+corpus
 
-corpus = filterDate(corpus, "2001-01-01", "2024-01-01") # falls nur ein bestimmter Zeitabschnitt gewuenscht ist
-corpus = clean_complete(corpus) # fuehrt ein Standar-Cleaning fuer den Korpus durch
+# filter by date
+corpus = filterDate(corpus, "1975-01-01", "1995-01-01") # falls nur ein bestimmter Zeitabschnitt gewuenscht ist
+
+# clean corpus automatically
+corpus = clean_complete(corpus) # fuehrt ein Standard-Cleaning fuer den Korpus durch
 
 
 # -------------------------------------------------------------------------
 # teilautomatisches cleanen
 # -------------------------------------------------------------------------
-
-
-require(reshape2)
-require(ggplot2)
 
 
 # check resources ---------------------------------------------------------
@@ -25,10 +27,10 @@ require(ggplot2)
 table(corpus$meta$resource)
 
 # plot sources:
-plotSources(corpus, alpha=0.6, span=0.5)
+plotSources(corpus, unit="year", alpha=0.6, smooth=F)
 
 # Quellen beibehalten:
-# quellen_list = c() # hier einen Vektor anlegen, der alle Quellennamen beinhaltet, die behalten werden sollen
+quellen_list = c("wikipedia") # hier einen Vektor anlegen, der alle Quellennamen beinhaltet, die behalten werden sollen
 corpus = filterID(corpus, corpus$meta$id[corpus$meta$resource %in% quellen_list])
 
 
@@ -38,7 +40,7 @@ corpus = filterID(corpus, corpus$meta$id[corpus$meta$resource %in% quellen_list]
 most_frquent_titles = table(corpus$meta$title)[1:20]
 most_frquent_titles
 
-pattern = "Johnson" # Begriffe (Patternsuche), durch "|" getrennt
+pattern = "Life" # Begriffe (Patternsuche), durch "|" getrennt
 filterTitles(corpus, pattern, print=T)
 
 # Doks ausschließen, bei denen <pattern> im Title vorkommt
