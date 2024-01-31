12f688f328fb92867e9bc422561d2d3858c98430
Tobias Schmidt <106234803+Piece-Of-Schmidt@users.noreply.github.com>
2024-01-31 15:34:11 +0100

    Funktionen um Korpus-Cleaning ergaenzt

--- erweiterte Funktionen.R
+++ erweiterte Funktionen.R
@@ -2,6 +2,298 @@
 require(lubridate, quietly = T)
 require(tosca, quietly = T)
 require(writexl, quietly = T)
+require(dplyr, quietly = T)
+require(utf8, quietly = T)
+require(ggplot2, quietly = T)
+
+
+# -------------------------------------------------------------------------
+# KORPUS CLEANING
+# -------------------------------------------------------------------------
+
+
+filterTitles = function(corpus, pattern, ignore.case=F, print=F){
+  
+  before = nrow(corpus$meta)
+  
+  # find dups
+  mask = !tosca::filterWord(corpus$meta$title, pattern, ignore.case=ignore.case, out="bin")
+  
+  if(print) print(corpus$meta$title[!mask])
+  
+  # print
+  cat(sprintf("Kept %d out of %d articles | %d removed (%.2f%%)\n",
+              sum(mask), before, before - sum(mask), 100 * (before - sum(mask)) / before))
+  
+  # return
+  return(tosca::filterID(corpus, corpus$meta$id[mask]))
+}
+
+
+filterDups_titles = function(corpus, unit = "day", message=T) {
+  
+  # Sicherstellen, dass das Eingabeformat korrekt ist
+  # if (!unit %in% c("all", "days", "months", "years")) stop('unit must be one of c("all", "days", "months", "years")')
+  
+  before = nrow(corpus$meta)
+  
+  # corpus sortieren
+  corpus$meta = corpus$meta[order(corpus$meta$date),]
+  corpus$text = corpus$text[match(corpus$meta$id, names(corpus$text))]
+  
+  if(any(is.na(corpus$meta$date))){
+    message("NAs found in meta data. Corpus is restricted to non-NA cases.")
+    corpus = filterID(corpus, corpus$meta$id[!is.na(corpus$meta$date)])
+  }
+  
+  # Erstellung der Datumsgruppen
+  date_chunks = if (unit == "all") "2000-01-01" else unique(lubridate::floor_date(corpus$meta$date, unit))
+  floor_dates = if (unit == "all") rep("2000-01-01", nrow(corpus$meta)) else lubridate::floor_date(corpus$meta$date, unit)
+  
+  # find dups
+  dups = unlist(sapply(date_chunks, function(chunk) {
+    mask = floor_dates == chunk
+    duplicated(corpus$meta$title[mask])
+  }, simplify = T))
+  
+  # print
+  if(message){
+    cat(sprintf("Kept %d out of %d articles | %d removed (%.2f%%)\n",
+                sum(!dups), before, before - sum(!dups), 100 * (before - sum(!dups)) / before))
+  }
+  
+  # return
+  return(tosca::filterID(corpus, corpus$meta$id[!dups]))
+}
+
+
+filterDups_leads = function(corpus, checkFirstChars = 120, unit = "day", message=T) {
+  
+  # if (!is.list(corpus) || !'text' %in% names(corpus) || !'meta' %in% names(corpus)) {
+  #   stop("Invalid corpus format")
+  # }
+  
+  before = nrow(corpus$meta)
+  
+  # Vorbereitung der Daten
+  leads = substr(corpus$text, 1, checkFirstChars)
+  ids = names(corpus$text)
+  
+  if (unit != "all") {
+    dates = corpus$meta$date[match(ids, corpus$meta$id)]
+    
+    # Erstellen eines Datenrahmens
+    df = data.frame(leads, ids, dates)
+    
+    # Konvertieren Sie Datumsangaben je nach Einheit
+    df$dates = lubridate::floor_date(df$dates, unit)
+    
+    # find dups
+    ids = df %>% 
+      arrange(dates) %>% 
+      group_by(dates) %>% 
+      filter(!duplicated(leads)) %>% 
+      pull(ids)
+  } else {
+    ids = ids[!duplicated(leads)]
+  }
+  
+  # print
+  if(message){
+    cat(sprintf("Kept %d out of %d articles | %d removed (%.2f%%)\n",
+                length(ids), before, before - length(ids), 100 * (before - length(ids)) / before))
+    
+  }
+  
+  return(tosca::filterID(corpus, id = ids, filtermeta = TRUE))
+}
+
+
+
+# Plot Sources
+plotSources = function(corpus, unit="month", ..., span=0.1) {
+  
+  # count
+  data = corpus$meta %>%
+    mutate(date = floor_date(date, unit)) %>%
+    count(date, resource)
+  
+  # generate plot
+  print(ggplot(data, aes(date, n, fill=resource))+
+          geom_area(...)+
+          geom_smooth(span=span, se=F, alpha=0.8, color="grey2")+ 
+          theme_classic())
+  
+  invisible(data)
+}
+
+
+clean_complete = function(corpus,
+                          shorten_meta=T,
+                          sort_meta=T,
+                          min_text_length = 750,
+                          max_text_length = quantile(nchar(corpus$text), 0.99),
+                          check_dups_id=T,
+                          check_dups_duplist=T,
+                          check_dups_textleads=120,
+                          check_dups_titles=T,
+                          check_dups_textleads_unit="week",
+                          check_dups_titles_unit="day",
+                          clean_titles=T,
+                          utf8=F,
+                          hbweltsz=F
+){
+  
+  before_whole = length(corpus$text)
+  
+  # Shorten meta data
+  if(shorten_meta){
+    a = Sys.time()
+    cat("Shorten meta data")
+    corpus$metamult = NULL
+    corpus$meta = corpus$meta[, names(corpus$meta) %in% c("id","date","title","resource")]
+    diff = difftime(Sys.time(), a)
+    cat(" | Done. Time for computation:", round(diff,3), attr(diff, "unit"), "\n")
+  }
+  
+  # Reorder meta files
+  if(sort_meta){
+    a = Sys.time()
+    cat("Reorder meta files")
+    corpus$meta = corpus$meta[order(corpus$meta$date),]
+    corpus$text = corpus$text[match(corpus$meta$id, names(corpus$text))]
+    corpus$meta = corpus$meta[corpus$meta$id %in% names(corpus$text),]
+    diff = difftime(Sys.time(), a)
+    cat(" | Done. Time for computation:", round(diff,3), attr(diff, "unit"), "\n")
+  }
+  
+  # Restrict corpus to long texts
+  if(!is.null(min_text_length)){
+    a = Sys.time()
+    before = nrow(corpus$meta)
+    cat("Restrict corpus to long texts (min_length =", min_text_length, "\b)...")
+    mask = nchar(corpus$text) >= min_text_length
+    corpus = filterID(corpus, names(mask)[mask])
+    diff = difftime(Sys.time(), a)
+    cat(" Done. Time for computation:", round(diff,3), attr(diff, "unit"))
+    cat(sprintf(" | Kept %d out of %d articles / %d removed (%.2f%%)\n",
+                nrow(corpus$meta), before, before - nrow(corpus$meta), 100 * (before - nrow(corpus$meta)) / before))
+    
+  }
+  
+  # remove too long texts
+  if(!is.null(max_text_length)){
+    a = Sys.time()
+    before = nrow(corpus$meta)
+    cat("Restrict corpus to short texts (max_length =", max_text_length, "\b)...")
+    mask = nchar(corpus$text) <= max_text_length
+    corpus = filterID(corpus, names(mask)[mask])
+    cat(" Done. Time for computation:", round(diff,3), attr(diff, "unit"))
+    cat(sprintf(" | Kept %d out of %d articles / %d removed (%.2f%%)\n",
+                nrow(corpus$meta), before, before - nrow(corpus$meta), 100 * (before - nrow(corpus$meta)) / before))
+  }
+  
+  # Redefine resources
+  if(hbweltsz){
+    cat("Redefine resources...")
+    a = Sys.time()
+    corpus$meta$source_indicator[!is.na(corpus$meta$source)] = "HB"
+    corpus$meta$source_indicator[!is.na(corpus$meta$resource)] = "Welt"
+    corpus$meta$source_indicator[!is.na(corpus$meta$AnzChar)] = "SZ"
+    corpus$meta$resource = corpus$meta$source_indicator
+    diff = difftime(Sys.time(), a)
+    cat(" Done. Time for computation:", round(diff,3), attr(diff, "unit"), "\n")
+  }
+  
+  # Convert texts and titles to utf8
+  if(utf8){
+    a = Sys.time()
+    cat("Convert texts and titles to utf8...")
+    corpus$text = lapply(corpus$text, utf8::as_utf8)
+    corpus$meta$title = sapply(corpus$meta$title, utf8::as_utf8)
+    diff = difftime(Sys.time(), a)
+    cat(" Done. Time for computation:", round(diff,3), attr(diff, "unit"), "\n")
+  }
+  
+  # -------------------------------------------------------------------------
+  # Duplikate raus
+  # -------------------------------------------------------------------------
+  
+  # Select unique text elements by ID
+  if(check_dups_id){
+    a = Sys.time()
+    before = nrow(corpus$meta)
+    cat("Select unique text elements by ID...")
+    ids = unique(names(corpus$text))
+    corpus$text = corpus$text[ids]
+    corpus$meta = corpus$meta[match(ids,corpus$meta$id),]
+    diff = difftime(Sys.time(), a)
+    cat(" Done. Time for computation:", round(diff,3), attr(diff, "unit"))
+    cat(sprintf("| Kept %d out of %d articles / %d removed (%.2f%%)\n",
+                nrow(corpus$meta), before, before - nrow(corpus$meta), 100 * (before - nrow(corpus$meta)) / before))
+    
+  }
+  
+  # duplist
+  if(check_dups_duplist){
+    a = Sys.time()
+    before = nrow(corpus$meta)
+    cat("Perform tosca::duplist() operation...")
+    invisible(capture.output(duplist <- suppressMessages(duplist(corpus))))
+    corpus = filterID(corpus, duplist$notDuplicatedTexts)
+    diff = difftime(Sys.time(), a)
+    cat(" Done. Time for computation:", round(diff,3), attr(diff, "unit"))
+    cat(sprintf(" | Kept %d out of %d articles / %d removed (%.2f%%)\n",
+                nrow(corpus$meta), before, before - nrow(corpus$meta), 100 * (before - nrow(corpus$meta)) / before))
+    
+  }
+  
+  # Filter Leads: Texte raus, die in den ersten X Zeichen uebereinstimmen
+  if(!is.null(check_dups_textleads)){
+    a = Sys.time()
+    before = nrow(corpus$meta)
+    cat("Filter Leads...")
+    corpus = filterDups_leads(corpus, check_dups_textleads, check_dups_textleads_unit, message=F)
+    diff = difftime(Sys.time(), a)
+    cat(" Done. Time for computation:", round(diff,3), attr(diff, "unit"))
+    cat(sprintf(" | Kept %d out of %d articles / %d removed (%.2f%%)\n",
+                nrow(corpus$meta), before, before - nrow(corpus$meta), 100 * (before - nrow(corpus$meta)) / before))
+  }
+  
+  # Filter Titles: Alle Texte raus, die am selben Tag erschienen sind und denselben Title haben
+  if(check_dups_titles){
+    a = Sys.time()
+    before = nrow(corpus$meta)
+    cat("Filter Titles...")
+    corpus = filterDups_titles(corpus, check_dups_titles_unit, message=F)
+    diff = difftime(Sys.time(), a)
+    cat(" Done. Time for computation:", round(diff,3), attr(diff, "unit"))
+    cat(sprintf(" | Kept %d out of %d articles / %d removed (%.2f%%)\n",
+                nrow(corpus$meta), before, before - nrow(corpus$meta), 100 * (before - nrow(corpus$meta)) / before))
+  }
+  
+  # Titles aufraeumen: Mehrfache Leerzeichen raus
+  if(clean_titles){
+    cat("Clean title strings...")
+    a = Sys.time()
+    corpus$meta$title = gsub("\\s+"," ",corpus$meta$title)
+    corpus$meta$title = gsub("&quot;","'",corpus$meta$title)
+    diff = difftime(Sys.time(), a)
+    cat(" Done. Time for computation:", round(diff,3), attr(diff, "unit"), "\n\n")
+  }
+  
+  after = length(corpus$text)
+  message(sprintf("Complete.\nKept %d out of %d articles / %d removed (%.2f%%)\n",
+                  after, before_whole, before_whole - after, 100 * (before_whole - after) / before_whole))
+  
+  return(corpus)
+}
+
+
+# -------------------------------------------------------------------------
+# LDA AUSWERTUNG
+# -------------------------------------------------------------------------
+
 
 #' Erweiterung von tosca::filterCounts(): erlaubt sowohl relative als auch absolute Werte, sowohl obere als auch untere Grenzen
 filterWordCounts = function(corpus, lower_thresh=0, upper_thresh=1){
@@ -13,31 +305,10 @@ filterWordCounts = function(corpus, lower_thresh=0, upper_thresh=1){
   
   # return restricted corpus
   mask = lens >= lower_thresh & lens <= upper_thresh
-  invisible(filterID(corpus, names(mask)[mask]))
+  return(filterID(corpus, names(mask)[mask]))
   
 }
 
-#' Erzeugt Samples basierend auf einem Text-Input. Input muss als Liste von Textsammlungen uebergeben werden.
-#' Funktion erlaubt den Einsatz verschiedener Speicher-Funktion. Eroeglicht damit den flexiblen Einsatz von
-#' write.csv(), write.csv2() für Windows-Files oder write.table(). 
-sample_texts = function(texts, sample_size=100, seed=1337, filenames=NULL, call="write.csv", ...){
-  
-  # build texts
-  lapply(seq(texts), function(idx){
-    
-    cat("Process subcorp ", idx, "/", length(texts), "\n", sep="")
-    
-    set.seed(seed)
-    s_texts = sample(texts[[idx]], size=sample_size)
-    
-    if(!is.null(filenames)){
-      filename = paste0(sub("[.]csv","",filenames[idx]), ".csv")
-      args = list(..., x = s_texts, file=filename)
-      do.call(call, args = args)
-    }
-    s_texts
-  })
-}
 
 #'lda_getTopTexts
 #'
@@ -287,6 +558,200 @@ topWordsPerUnit = function(corpus, ldaresult, docs, unit="quarter", numWords=50,
   }
   
   invisible(out)
+}
+
+
+multipleLDAs = function(docs, vocab, ..., func = "LDAgen", runs="all", seed=1337, savelogs = T){
+  
+  onlyonedoc = length(unique(lengths(docs))) != 1
+  onlyonevoc = !is.list(vocab)
+  
+  stopifnot(onlyonedoc == onlyonevoc)
+  
+  model = if(onlyonedoc) 1 else seq(docs)
+  
+  lda_vars = list(model=model, ...)
+  
+  # build grid
+  grid = expand.grid(lda_vars)
+  
+  # take sample
+  if(runs != "all"){
+    set.seed(seed)
+    runs = min(nrow(grid), runs)
+    grid = grid[sample(1:nrow(grid), runs), ]
+  }
+  
+  cat("Model params (", runs, "):\n", sep="")
+  print(grid)
+  
+  out = lapply(seq(nrow(grid)), function(idx){
+    
+    args = grid[idx,]
+    cat("\nCalculate LDA: ", paste(names(args), args, sep=": ", collapse = ", "))
+    
+    d = if(onlyonedoc) docs else docs[[unlist(args[1])]]
+    v = if(onlyonevoc) vocab else vocab[[unlist(args[1])]]
+    
+    args = as.list(args[,-1])
+    args$documents = d
+    args$vocab = v
+    
+    do.call(func, args = args)
+    
+  })
+  
+  model_ids = paste0("run", 1:nrow(grid))
+  names(out) = model_ids
+  
+  if(savelogs){
+    grid = data.frame(id = model_ids, grid)
+    write.csv(grid, "model_logs.csv")
+  }
+  
+  invisible(out)
+  
+}
+
+decompose_lda = function(document_topic_matrix, lookup_dict, select=1:nrow(document_topic_matrix), unit="month", tnames=paste0("topic", select), plot=F, out="melt"){
+  
+  # LOOKUP-DICT: create floor_dates
+  colnames(lookup_dict) = c("id", "date", "group")
+  lookup_dict$date = lubridate::floor_date(lookup_dict$date, unit=unit)
+  
+  # DTM: create rownames & colnames and restrict DTM to selected topics
+  colnames(document_topic_matrix) = lookup_dict$id
+  document_topic_matrix = document_topic_matrix[select, , drop=F]
+  rownames(document_topic_matrix) = tnames
+  
+  # DTM: create long format
+  long_format = reshape2::melt(document_topic_matrix)
+  colnames(long_format) = c("topic", "id", "count")
+  
+  # DTM: merge with LOOKUP-DICT
+  merged_data = long_format %>%
+    left_join(lookup_dict, by = "id") %>%
+    group_by(date, group, topic) %>%
+    summarise(doc_count = sum(count))
+  
+  # FINAL: create plot
+  if(plot){
+    print(ggplot2::ggplot(merged_data, aes(x = date, y = doc_count, color = group)) +
+            geom_smooth(se=F, span=0.1) +
+            facet_wrap(~ topic) +
+            theme_minimal() +
+            labs(x = "Monat", y = "Anzahl Dokumente", color = "Quelle"))
+  }
+  
+  result = if(out == "melt") merged_data else dcast(merged_data, date + group ~ topic, value.var = "doc_count")
+  
+  return(result)
+  
+}
+
+
+
+#' Erzeugt Samples basierend auf einem Text-Input. Input muss als Liste von Textsammlungen uebergeben werden.
+#' Funktion erlaubt den Einsatz verschiedener Speicher-Funktionen. Eroeglicht damit den flexiblen Einsatz von
+#' write.csv(), write.csv2() für Windows-Files oder write.table(). 
+sample_texts = function(texts, sample_size=100, seed=1337, filenames=NULL, call="write.csv", ...){
+  
+  # build texts
+  lapply(seq(texts), function(idx){
+    
+    cat("Process subcorp ", idx, "/", length(texts), "\n", sep="")
+    
+    set.seed(seed)
+    s_texts = sample(texts[[idx]], size=sample_size)
+    
+    if(!is.null(filenames)){
+      filename = paste0(sub("[.]csv","",filenames[idx]), ".csv")
+      args = list(..., x = s_texts, file=filename)
+      do.call(call, args = args)
+    }
+    s_texts
+  })
+}
+
+
+
+# -------------------------------------------------------------------------
+# TEXT ANALYSIS FUNCS
+# -------------------------------------------------------------------------
+
+#' Erzeugt Samples basierend auf einem Text-Input. Input muss als Liste von Textsammlungen uebergeben werden.
+#' Funktion erlaubt den Einsatz verschiedener Speicher-Funktion. Eroeglicht damit den flexiblen Einsatz von
+#' write.csv(), write.csv2() für Windows-Files oder write.table(). 
+sample_texts = function(texts, sample_size=100, seed=1337, filenames=NULL, call="write.csv", ...){
+  
+  # build texts
+  lapply(seq(texts), function(idx){
+    
+    cat("Process subcorp ", idx, "/", length(texts), "\n", sep="")
+    
+    set.seed(seed)
+    s_texts = sample(texts[[idx]], size=sample_size)
+    
+    if(!is.null(filenames)){
+      filename = paste0(sub("[.]csv","",filenames[idx]), ".csv")
+      args = list(..., x = s_texts, file=filename)
+      do.call(call, args = args)
+    }
+    s_texts
+  })
+}
+
+
+highlight = function(text, pattern, color=31){
+  if(grepl("\\|",pattern)) pattern = strsplit(pattern,"|",fixed=T)[[1]]
+  if(length(text)>1) text = paste(text, collapse = "\n\n")
+  for(pat in pattern) text = gsub(pat,paste0("\033[0;",color,"m",pat,"\033[0m"), text)
+  return(cat(text))
+}
+
+
+get_context = function(texts, pattern, windowsize=30, seperator=NULL, ignore.case=F, perl=F, offset=T){
+  
+  out = sapply(texts, function(text){
+    
+    # find target word
+    match = regexpr(pattern, text, ignore.case=ignore.case, perl=perl)
+    
+    # if target word is found do:
+    if(match[1]!=-1){
+      
+      # tokenize text
+      words = gregexpr("\\b\\w+\\b", text)[[1]]
+      
+      # only keep words around target word
+      keep = words[words > match-windowsize & words < match+windowsize]
+      
+      first = keep[1] # first word to keep
+      last = keep[length(keep)] # last word to keep
+      
+      # add word length to last word index so it is part of the context
+      last = last + attr(words, "match.length")[match(last, words)]
+      
+      # actually restrict context to rel context and remove whitespace
+      keep = gsub("\\s$","",substr(text, first, last))
+      
+      # highlight target word
+      if(!is.null(seperator)) sub(pattern, paste(seperator, pattern, seperator), keep) else keep
+      
+    }else ""
+    
+  }, USE.NAMES = F)
+  
+  if(offset){
+    # center texts
+    offsets = regexpr(pattern, out)
+    center = max(offsets)
+    
+    # return
+    out = paste0(strrep(" ", center-offsets), out)
+  }
+  return(out)
+  
 }
 
 
