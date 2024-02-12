37662e8bc5e0e9b1e00c93248200fb81f4fa0e81
Tobias Schmidt <106234803+Piece-Of-Schmidt@users.noreply.github.com>
2024-02-12 12:41:42 +0100

    Update 0_functions.R

--- 0_functions.R
+++ 0_functions.R
@@ -123,7 +123,7 @@ filterDups_leads = function(corpus, checkFirstChars = 120, unit = "day", message
 
 
 # Plot Sources
-plotSources = function(corpus, unit="month", area_alpha=0.6, area_position="dodge", ..., smooth=T, span=0.1) {
+plotSources = function(corpus, unit="month", area_alpha=0.6, area_position="identity", ..., smooth=T, span=0.1) {
   
   # count
   data = corpus$meta %>%
@@ -138,7 +138,7 @@ plotSources = function(corpus, unit="month", area_alpha=0.6, area_position="dodg
       theme_classic()
   }else{
     p = ggplot(data, aes(date, n, fill=resource))+
-      geom_area(...)+
+      geom_area(alpha=area_alpha, position=area_position,...)+
       theme_classic()
   }
   print(p)
