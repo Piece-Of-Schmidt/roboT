6eb265c6cd599a97a29dca150d16cdcccd5b4cb3
Tobias Schmidt <106234803+Piece-Of-Schmidt@users.noreply.github.com>
2024-02-13 10:47:32 +0100

    Update 0_functions.R

--- 0_functions.R
+++ 0_functions.R
@@ -684,7 +684,45 @@ topWordsPerUnit = function(corpus, ldaresult, docs, unit="quarter", numWords=50,
   invisible(out)
 }
 
+decompose_lda = function(document_topic_matrix, lookup_dict, select=1:nrow(document_topic_matrix), unit="month", tnames=paste0("topic", select), plot=F, out="melt"){
+  
+  # LOOKUP-DICT: create floor_dates
+  colnames(lookup_dict) = c("id", "date", "group")
+  lookup_dict$date = lubridate::floor_date(lookup_dict$date, unit=unit)
+  
+  # DTM: create rownames & colnames and restrict DTM to selected topics
+  colnames(document_topic_matrix) = lookup_dict$id
+  document_topic_matrix = document_topic_matrix[select, , drop=F]
+  rownames(document_topic_matrix) = tnames
+  
+  # DTM: create long format
+  long_format = reshape2::melt(document_topic_matrix)
+  colnames(long_format) = c("topic", "id", "count")
+  
+  # DTM: merge with LOOKUP-DICT
+  merged_data = long_format %>%
+    left_join(lookup_dict, by = "id") %>%
+    group_by(date, group, topic) %>%
+    summarise(doc_count = sum(count))
+  
+  # FINAL: create plot
+  if(plot){
+    print(ggplot2::ggplot(merged_data, aes(x = date, y = doc_count, color = group)) +
+            geom_smooth(se=F, span=0.1) +
+            facet_wrap(~ topic) +
+            theme_minimal() +
+            labs(x = "Monat", y = "Anzahl Dokumente", color = "Quelle"))
+  }
+  
+  result = if(out == "melt") merged_data else dcast(merged_data, date + group ~ topic, value.var = "doc_count")
+  
+  return(result)
+  
+}
 
+
+
+# perform multiple LDAs with different settings
 multipleLDAs = function(docs, vocab, ..., func = "LDAgen", runs="all", seed=1337, savelogs = T){
   
   onlyonedoc = length(unique(lengths(docs))) != 1
@@ -737,74 +775,12 @@ multipleLDAs = function(docs, vocab, ..., func = "LDAgen", runs="all", seed=1337
   
 }
 
-decompose_lda = function(document_topic_matrix, lookup_dict, select=1:nrow(document_topic_matrix), unit="month", tnames=paste0("topic", select), plot=F, out="melt"){
-  
-  # LOOKUP-DICT: create floor_dates
-  colnames(lookup_dict) = c("id", "date", "group")
-  lookup_dict$date = lubridate::floor_date(lookup_dict$date, unit=unit)
-  
-  # DTM: create rownames & colnames and restrict DTM to selected topics
-  colnames(document_topic_matrix) = lookup_dict$id
-  document_topic_matrix = document_topic_matrix[select, , drop=F]
-  rownames(document_topic_matrix) = tnames
-  
-  # DTM: create long format
-  long_format = reshape2::melt(document_topic_matrix)
-  colnames(long_format) = c("topic", "id", "count")
-  
-  # DTM: merge with LOOKUP-DICT
-  merged_data = long_format %>%
-    left_join(lookup_dict, by = "id") %>%
-    group_by(date, group, topic) %>%
-    summarise(doc_count = sum(count))
-  
-  # FINAL: create plot
-  if(plot){
-    print(ggplot2::ggplot(merged_data, aes(x = date, y = doc_count, color = group)) +
-            geom_smooth(se=F, span=0.1) +
-            facet_wrap(~ topic) +
-            theme_minimal() +
-            labs(x = "Monat", y = "Anzahl Dokumente", color = "Quelle"))
-  }
-  
-  result = if(out == "melt") merged_data else dcast(merged_data, date + group ~ topic, value.var = "doc_count")
-  
-  return(result)
-  
-}
-
-
-
-#' Erzeugt Samples basierend auf einem Text-Input. Input muss als Liste von Textsammlungen uebergeben werden.
-#' Funktion erlaubt den Einsatz verschiedener Speicher-Funktionen. Eroeglicht damit den flexiblen Einsatz von
-#' write.csv(), write.csv2() für Windows-Files oder write.table(). 
-sample_texts = function(texts, sample_size=100, seed=1337, filenames=NULL, call="write.csv", ...){
-  
-  # build texts
-  lapply(seq(texts), function(idx){
-    
-    cat("Process subcorp ", idx, "/", length(texts), "\n", sep="")
-    
-    set.seed(seed)
-    s_texts = sample(texts[[idx]], size=sample_size)
-    
-    if(!is.null(filenames)){
-      filename = paste0(sub("[.]csv","",filenames[idx]), ".csv")
-      args = list(..., x = s_texts, file=filename)
-      do.call(call, args = args)
-    }
-    s_texts
-  })
-}
-
-
-
 # -------------------------------------------------------------------------
 # TEXT ANALYSIS FUNCS
 # -------------------------------------------------------------------------
 
 #' Erzeugt Samples basierend auf einem Text-Input. Input muss als Liste von Textsammlungen uebergeben werden.
-#' Funktion erlaubt den Einsatz verschiedener Speicher-Funktion. Eroeglicht damit den flexiblen Einsatz von
+#' Funktion erlaubt den Einsatz verschiedener Speicher-Funktionen. Ermoeglicht damit den flexiblen Einsatz von
 #' write.csv(), write.csv2() für Windows-Files oder write.table(). 
 sample_texts = function(texts, sample_size=100, seed=1337, filenames=NULL, call="write.csv", ...){
   
