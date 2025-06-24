fc4bddcd575847f1129a4d0e2c2acded53c1d338
Tobias Schmidt <106234803+Piece-Of-Schmidt@users.noreply.github.com>
2025-06-24 11:47:15 +0200

    Delete 0_functions.R

--- 0_functions.R
+++
@@ -1,6 +0,0 @@
-# set wd
-setwd(dirname(rstudioapi::getSourceEditorContext()$path))
-
-# read script files
-script_files = list.files(path="functions", pattern=".R", full.names=T)
-for(file in script_files) source(file)
