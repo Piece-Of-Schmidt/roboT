5f92bbf1ae5501284f215e86e8ab2e80d610806e
Tobias Schmidt <106234803+Piece-Of-Schmidt@users.noreply.github.com>
2025-10-29 11:06:35 +0100

    Update 2_tosca_LDA_basics.R
    skript verbessert

--- scripts/2_tosca_LDA_basics.R
+++ scripts/2_tosca_LDA_basics.R
@@ -54,35 +54,48 @@ saveRDS(docs, "docs.rds") # -> Objekt speichern. Das brauchen wir spaeter noch b
 
 
 # -------------------------------------------------------------------------
-# LDA rechnen
+# LDA rechnen und auswerten
 # -------------------------------------------------------------------------
 
-K_WERTE = c(8,10) # <- probieren Sie hier verschiedene K-Werte aus
+K_WERTE = c(8, 10) # <- probieren Sie hier verschiedene K-Werte aus, z.B. K_WERTE = c(4, 6, 8) oder K_WERTE = seq(10, 20, 2), oder K_WERTE = seq(10, 60, 10), ...
 
+# define seed for LDA
+seed = 123 # arbitrary choice
+
+# select sample dates for top texts per quarter <- an Korpus-Größe (min-Date, max-Date) anpassen
+rel_dates = as.Date(c("2001-04-01", "2005-06-01", "2010-07-01", "2013-10-01", "2016-05-01", "2019-06-01", "2022-08-01", "2025-01-01")) # arbitrary selection
+
+# loop through all k values
 for(k in K_WERTE){
   
   message("Generate results for K=", k)
 
-  # define seed
-  seed = 123
-  
   # create subfolder
   if(! dir.exists(paste0("K",k))) dir.create(paste0("K",k))
   
   # save LDA-result to R environment
-  result = LDAgen(docs, K=k, vocab, folder = paste0("K",k,"/Ergebnis"), seed = seed)
+  result = LDAgen(docs, K=k, vocab, folder = paste0("K",k,"/Ergebnis"), seed=seed)
   assign(paste0("result_K",k), result)
   
   # save LDA-result locally
   saveRDS(result, sprintf("K%d/result_K%d_seed%d.rds", k, k, seed))
   
   # create Top Words
-  lda_getTopWords(result, file = sprintf("K%d/TopWords_K%d_seed%d.rds", k, k, seed))
+  lda_getTopWords(result, file = sprintf("K%d/TopWords_K%d_seed%d", k, k, seed))
   
   # create Top Texts
-  lda_getTopTexts(corp, result, ldaID = names(docs), file = sprintf("K%d/TopTexts_K%d_seed%d.rds", k, k, seed))
-  
-}
+  lda_getTopTexts(corp, result, ldaID = names(docs), file = sprintf("K%d/TopTexts_K%d_seed%d", k, k, seed))
+
+  # extract top words per quarter 
+  lda_getTopWordsPerUnit(corpus = corp, ldaresult = result, docs = docs, unit = "quarter", file = sprintf("K%d/TopWordsPerQuarter_k%s_seed%s", k, k, seed))
+
+  # extract top texts per quarter -> most relevant docs per quarter per topic
+  lda_getTopTextsPerUnit(corpus = corp, ldaresult = result, ldaID = names(docs), unit = "quarter", groupby = "topic", select_dates = rel_dates, foldername = sprintf("K%d/TopTextsPerQuarter", k, k, seed))
+
+  # plot topics
+  plotTopic(object = tokenizedCorpus, ldaresult = result, ldaID = names(docs), rel = FALSE, curves = "both", smooth = 0.1, unit = "quarter", legend = "topleft", pages = TRUE, file = sprintf("K%d/topics_over_time_K%d_seed%d.pdf", k, k, seed))
+
+}; message("files are stored in: ", getwd())
 
 
 
@@ -94,30 +107,14 @@ for(k in K_WERTE){
 # Weitere Funktionen fuer die Auswertung: Siehe Koppers et al. (2021) tosca Vignette
 
 
-# print TopWords along with Doc Titles
-get_tw_and_titles(corpus = corp, ldaresult = result_K8, ldaID = names(docs), topic = 8, n = 20)
+# print TopWords along with Doc Titles (e.g. for K=8)
+get_tw_and_titles(corpus = corp, ldaresult = result_K8, ldaID = names(docs), n = 20)
 
 
-# Beispiel: PlotTopic:
-plotTopic(object = tokenizedCorpus, ldaresult = result_K8, ldaID = names(docs),
-          select = c(3,4), rel = TRUE, curves = "both", smooth = 0.1, legend = "topleft")
-
-# um die Plots lokal zu speichern:
+# um die Plots lokal zu speichern (e.g. for K=8)
 plot = plotTopic(object = tokenizedCorpus, ldaresult = result_K8, ldaID = names(docs),
                  select = c(3,4), rel = TRUE, curves = "both", smooth = 0.1, legend = "topleft")
 write.csv2(plot, "plot.csv")
-
-
-# Top Texts per unit (hier: quarter) - pro Quartal die relevantesten Texte pro Topic
-K_value = 8
-result = get(paste0("result_K", K_value))
-lda_getTopTextsPerUnit(corpus = corp, ldaresult = result, ldaID = names(docs), unit = "quarter", groupby="topic", foldername = paste0("K", K_value, "/TopTextsPerUnit"))
-
-# Top Words per unit (hier: quarter) - pro Quartal die relevantesten Worte pro Topic
-K_value = 8
-result = get(paste0("result_K", K_value))
-lda_getTopWordsPerUnit(corpus = corp, ldaresult = result, docs = docs, unit = "quarter", groupby="topic", file = paste0("K", K_value, "/TopWordsPerUnit"))
-
 
 
 
@@ -143,5 +140,6 @@ Ausserdem werden auf diese Weise auch unnoetige Objekte mit gespeichert.
 # save everything
 current_time = gsub(":","-",format(Sys.time(), "%X"))
 save.image(paste0("analysis_", current_time, ".RData"))
+
 
 
